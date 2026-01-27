3455bb607664647d2b63c54cbaba4d859527f4c1
PedroHPG <pedrohpgoncalves@hotmail.com>
2026-01-27 10:32:58 -0300

    Project outline

--- Calc/src/main/java/InterestCalculator/Calc/App.java
+++ Calc/src/main/java/InterestCalculator/Calc/App.java
@@ -1,13 +1,26 @@
 package InterestCalculator.Calc;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	//Initialize scanner
+    	
+    	//Take input on operation type
+        System.out.println( "Enter the desired operation (s for simple interest, c for compound interest, anything else to end): " );
+        
+        //Take input on interest rate
+        
+        //Take input on starting amount
+        
+        //Take input on time
+        
+        //Calculate simple interest
+        
+        //Calculate compound interest
+        
+        //Return calculated value and ask for further operations
+        
     }
 }
