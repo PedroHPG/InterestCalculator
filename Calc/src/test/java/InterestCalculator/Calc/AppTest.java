e398e2cef3d0fb7e87cfb7f6b54d24853ea1c7c8
PedroHPG <pedrohpgoncalves@hotmail.com>
2026-02-02 11:12:50 -0300

    Added test for simple interest

--- Calc/src/test/java/InterestCalculator/Calc/AppTest.java
+++ Calc/src/test/java/InterestCalculator/Calc/AppTest.java
@@ -1,9 +1,12 @@
 package InterestCalculator.Calc;
 
 import static org.junit.Assert.assertThat;
-
+import InterestCalculator.Calc.*;
+import org.junit.Assert;
 import org.junit.Test;
-
+import org.junit.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matcher.*;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
@@ -12,7 +15,7 @@ public class AppTest
     extends TestCase
 {
     @Test
-    public void testTODO() {
-    	
+    public void testSimpleInterestCalculation() {
+    	assertEquals(App.interestCalculation('s', 10, 100, 10), 100.00);
     }
 }
